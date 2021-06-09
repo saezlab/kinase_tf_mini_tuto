9fb84ed8f929560dc62365d286d2a4fa4acc392b
Martin Garrido Rodriguez-Cordoba <mgrcprof@gmail.com>
2021-06-09 17:10:43 +0200

    wip

--- code/reproduce_analysis.R
+++ code/reproduce_analysis.R
@@ -2,4 +2,4 @@ if( !require('renv') ) {
   install.packages('renv')
 }
 renv::restore()
-source(knitr::purl("analysis/index.Rmd"), output = tempfile())
+source(knitr::purl("analysis/index.Rmd", output = tempfile()))
